a3050d55c2db868b5cd4497d3ce2fbacfcd710ef
Niek Schoemaker <32094562+niekschoemaker@users.noreply.github.com>
2021-05-04 23:14:19 +0200

    Remove unecessary scripts from manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,6 @@ game 'gta5'
 use_fxv2_oal 'true'
 
 client_scripts {
-    '@esx_boilerplate/natives.lua',
-    '@es_extended/locale.lua',
     'controls.lua',
     'deathevents.lua',
     'vehiclechecker.lua',
@@ -12,8 +10,6 @@ client_scripts {
 }
 
 server_scripts {
-    '@esx_boilerplate/natives_server.lua',
-    '@anticheat/event_s.lua',
     'server.lua'
 }
 
